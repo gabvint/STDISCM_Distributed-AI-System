e3153b4cd16c5fa6c13bea2b5a8af06356543f1e
Shanky de Gracia <shankydegracia@gmail.com>
2025-12-04 17:03:06 +0800

    proto

--- server/generated/ocr.grpc.pb.cc
+++ server/generated/ocr.grpc.pb.cc
@@ -19,7 +19,6 @@
 #include <grpcpp/server_context.h>
 #include <grpcpp/impl/service_type.h>
 #include <grpcpp/support/sync_stream.h>
-#include <grpcpp/ports_def.inc>
 namespace ocr {
 
 static const char* OcrService_method_names[] = {
@@ -84,5 +83,4 @@ OcrService::Service::~Service() {
 
 
 }  // namespace ocr
-#include <grpcpp/ports_undef.inc>
 
